aa5fcbe2cd0d6ce4ebdca3287cbf93194a4c4ac1
priscilamarques <prisoli_marques@hotmail.com>
2020-06-24 22:11:19 -0400

    First practice

--- primeiro_comando.py
+++ primeiro_comando.py
@@ -20,6 +20,7 @@ print('Você nasceu no dia',dia,'de',mes,'do ano de',ano,'.''Correto?')
 
 ##Desafio 3
 #Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
 n1 = int(input("Primeiro número: "))
 n2 = int(input("Segundo número: "))
 soma = n1 + n2
