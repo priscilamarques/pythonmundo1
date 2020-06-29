fc17f3f33eba52bee92ed4b8995274726d1f90c8
priscilamarques <prisoli_marques@hotmail.com>
2020-06-28 21:06:39 -0400

    Change desafio8

--- desafio8.py
+++ desafio8.py
@@ -2,6 +2,6 @@
 #Escreva um programa que leia o valor em metros e o exiba convertido em centímetros e milímetros.
 
 m = float(input('Quantos metros? '))
-cm = m/100
-mm = m/1000
+cm = m*100
+mm = m*1000
 print('Metro(s) {}, centímetros {} e milímetros {}'.format(m,cm,mm))
