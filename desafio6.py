ea401f33ce4ebebd96eded5ec378d05b7303daef
priscilamarques <prisoli_marques@hotmail.com>
2020-06-30 17:26:30 -0400

    Code improvement and correction

--- desafio6.py
+++ desafio6.py
@@ -4,5 +4,5 @@
 n = int(input('Digite um número: '))
 d = n * 2
 t = n * 3
-e = n ** 2
+e = n ** (1/2)
 print('O número é {}. O dobro é {}, o triplo é {} e a raiz quadrada é {}.'.format(n,d,t,e))
--- desafio8.py
+++ desafio8.py
@@ -4,4 +4,4 @@
 m = float(input('Quantos metros? '))
 cm = m*100
 mm = m*1000
-print('Metro(s) {}, centímetros {} e milímetros {}'.format(m,cm,mm))
+print('{:.0f} metros corresponde a {:.0f} centímetros e {:.0f} milímetros '.format(m,cm,mm))
